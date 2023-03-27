95bf6f42a416de7e76cb1c2c248a5f1f96ce92dc
clementine delthe <clementine.delthe@gmail.com>
2023-03-27 16:31:45 +0200

    add dotenv-rails

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,38 @@
+class ListsController < ApplicationController
+  before_action :set_params, only: %i[show destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:id, :name)
+  end
+
+  def set_params
+    @list = List.find(params[:id])
+  end
+end
